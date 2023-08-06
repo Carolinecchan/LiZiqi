96d483458cb2e8b5de74d4388803a334e0b29795
Nicholas Chen <nc1020@ic.ac.uk>
2023-08-06 21:08:24 +0100

    modify liziqi.py

--- liziqi.py
+++ liziqi.py
@@ -1,18 +1,36 @@
 import pandas as pd
+import os
 
 
-def data_analysis(table_path: str, keyword: str) -> pd.DataFrame:
-    df = pd.read_csv(table_path)
+def data_analysis(table_path: list, keyword: str) -> int:
+    df1 = pd.read_csv(table_path[0])
+    df2 = pd.read_csv(table_path[1])
     column_name = '含' + keyword
-    non_nan = ~df["中文翻译"].isna()
-    df.loc[(df["中文翻译"].str.contains(keyword, regex=False)) & non_nan, column_name] = df['中文翻译']
-    return df[column_name].dropna()
+    column_chinese = column_name + '中文翻译'
+    non_nan_1 = ~df1["英文翻译"].isna()
+    df1.loc[(df1["英文翻译"].str.contains(keyword, regex=False)) & non_nan_1, column_name] = df1['英文翻译']
+    df1.loc[(df1["英文翻译"].str.contains(keyword, regex=False)) & non_nan_1, column_chinese] = df1['中文翻译']
+    non_nan_2 = ~df2["英文翻译"].isna()
+    df2.loc[(df2["英文翻译"].str.contains(keyword, regex=False)) & non_nan_2, column_name] = df2['英文翻译']
+    df2.loc[(df2["英文翻译"].str.contains(keyword, regex=False)) & non_nan_2, column_chinese] = df2['中文翻译']
+    re_df1 = df1.loc[:, ['cid', 'votes', column_name, column_chinese]].dropna()
+    re_df2 = df2.loc[:, ['cid', 'votes', column_name, column_chinese]].dropna()
+    re_df1 = re_df1.sort_values(by='votes', ascending=False)
+    re_df2 = re_df2.sort_values(by='votes', ascending=False)
+    sum1 = str(len(re_df1))
+    sum2 = str(len(re_df2))
+    sum = sum1 + sum2
+    os.makedirs(column_name, exist_ok=True)
+    path1_csv = column_name + '/New' + sum1 + column_name + '.csv'
+    path1_xlsx = column_name + '/New' + sum1 + column_name + '.xlsx'
+    path2_csv = column_name + '/Luo' + sum2 + column_name + '.csv'
+    path2_xlsx = column_name + '/Luo' + sum2 + column_name + '.xlsx'
+    re_df1.to_csv(path1_csv)
+    re_df2.to_csv(path2_csv)
+    re_df1.to_excel(path1_xlsx)
+    re_df2.to_excel(path2_xlsx)
+    return sum
 
 
-def comment_sum(df1: pd.DataFrame, df2: pd.DataFrame) -> int:
-    return len(df1)+len(df2)
-
-
-print(data_analysis('Liuzhou Luosifen翻译.csv','美食').to_string())
-print(data_analysis('New Year snacks 翻译.csv','美食').to_string())
-print('评论总数： ',comment_sum(data_analysis('Liuzhou Luosifen翻译.csv', '美食'), data_analysis('New Year snacks 翻译.csv', '美食')))
+list_file = ['New Year snacks 翻译.csv', 'Liuzhou Luosifen翻译.csv']
+data_analysis(list_file, 'culture')
