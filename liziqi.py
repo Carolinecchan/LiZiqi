03095844949f39d60ce412e87b287c352e97c552
Nicholas Chen <nc1020@ic.ac.uk>
2023-08-06 19:39:24 +0100

    modify liziqi.py

--- liziqi.py
+++ liziqi.py
@@ -4,5 +4,5 @@ def data_analysis(table_path: str, keyword: str) -> pd.DataFrame:
     df = pd.read_excel(table_path)
     column_name = '含' + keyword
     non_nan = ~df["中文翻译"].isna()
-    df.loc[(df["中文翻译"].str.contains(keyword,regex=False))&non_nan,column_name] = df['中文翻译']
+    df.loc[(df["中文翻译"].str.contains(keyword, regex=False)) & non_nan, column_name] = df['中文翻译']
     return df[keyword]
