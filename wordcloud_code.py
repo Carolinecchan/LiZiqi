4b631d58635cf28bc0bb9ceefb1d343a10f92f60
Nicholas Chen <nc1020@ic.ac.uk>
2023-08-06 22:43:46 +0100

    modify wordcloud

--- wordcloud_code.py
+++ wordcloud_code.py
@@ -8,8 +8,12 @@ import matplotlib.pyplot as plt
 
 df1 = pd.read_csv('New Year snacks 翻译.csv')
 df2 = pd.read_csv('Liuzhou Luosifen翻译.csv')
-text = " ".join(review for review in list(df1['英文翻译']))
-wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
+df1['英文翻译'] = df1['英文翻译'].astype(str)
+df2['英文翻译'] = df2['英文翻译'].astype(str)
+text1 = " ".join(review for review in df1['英文翻译'])
+text2 = " ".join(review for review in df2['英文翻译'])
+text = text1 + " " + text2
+wordcloud = WordCloud(max_font_size=100, max_words=300, background_color="white").generate(text)
 plt.figure()
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
