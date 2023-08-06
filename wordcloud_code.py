14411f323e75f9a9f2db3d3d13fc5b964cc848bb
Nicholas Chen <nc1020@ic.ac.uk>
2023-08-06 23:31:10 +0100

    modify wordcloud_code.py

--- wordcloud_code.py
+++ wordcloud_code.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 from os import path
-from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import stylecloud
 
 import matplotlib.pyplot as plt
 
@@ -13,9 +12,19 @@ df2['英文翻译'] = df2['英文翻译'].astype(str)
 text1 = " ".join(review for review in df1['英文翻译'])
 text2 = " ".join(review for review in df2['英文翻译'])
 text = text1 + " " + text2
-wordcloud = WordCloud(max_font_size=100, max_words=300, background_color="white").generate(text)
-plt.figure()
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-plt.show()
-wordcloud.to_file("first_review.png")
+# wordcloud = WordCloud(max_font_size=100, max_words=300, background_color="white").generate(text)
+# plt.figure()
+# plt.imshow(wordcloud, interpolation="bilinear")
+# plt.axis("off")
+# plt.show()
+# wordcloud.to_file("first_review.png")
+stylecloud.gen_stylecloud(text=text,
+                          size=512,
+                          icon_name='fas fa-heart',
+                          palette='cartocolors.sequential.Magenta_5',
+                          output_name='test_ciyun.png')
+# stylecloud.gen_stylecloud(text=text,
+#                           size=512,
+#                           icon_name='fas fa-heart',
+#                           palette='tableau.Tableau_20',
+#                           output_name='test_ciyun.png')
